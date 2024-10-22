e0b0c1718dd82912a0c34ed6604bf261c1d0ad6a
Alain Precioso <alainprecioso@gmail.com>
2024-10-22 14:15:36 +0200

    Refactor authentication check to use Auth facade

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    @if (auth()->user() && auth()->user()->role === 'admin')
+                    @if (Auth::user() && Auth::user()->role === 'admin')
                     <x-nav-link :href="route('admin')" :active="request()->routeIs('admin')">
                         {{ __('Admin') }}
                     </x-nav-link>
