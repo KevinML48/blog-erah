18d82e7e824ba65739f8ae4b9ad1d667267afc3f
Alain Precioso <alainprecioso@gmail.com>
2024-10-24 07:36:40 +0200

    Add a note to the credits for unpublished posts

--- resources/views/posts/partials/credit.blade.php
+++ resources/views/posts/partials/credit.blade.php
@@ -1,16 +1,21 @@
-Par
+<span>Par</span>
 <a href="{{ route('profile.show', ['username' => $post->user->name]) }}" class="text-blue-600 hover:underline">
     {{ $post->user->name }}
 </a>
-le
-<span class="convert-time" data-time="{{ $post->publication_time ? $post->publication_time->toIso8601String() : $post->created_at->toIso8601String() }}">
+<span>le</span>
+<span class="convert-time"
+      data-time="{{ $post->publication_time ? $post->publication_time->toIso8601String() : $post->created_at->toIso8601String() }}">
     <!-- Placeholder that will be replaced by JavaScript -->
 </span>
 
 @if (($post->publication_time && $post->publication_time->isBefore($post->updated_at)) ||
     (!$post->publication_time && $post->created_at->isBefore($post->updated_at)))
-    Edité le
+    <span>Édité le</span>
     <span class="convert-time" data-time="{{ $post->updated_at->toIso8601String() }}">
         <!-- Placeholder that will be replaced by JavaScript -->
     </span>
+@endif
+
+@if ($post->publication_time && $post->publication_time->isFuture())
+    <span>Non publié</span>
 @endif
