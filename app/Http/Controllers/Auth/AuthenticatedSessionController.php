da18bff6882b67e7eb0aaa96b47d08c23727c4b9
Alain Precioso <alainprecioso@gmail.com>
2024-10-25 06:16:49 +0200

    Change authentication redirection
    Admins are redirected to the admin dashboard and other users to the
    welcome page

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return auth()->user()->role === 'admin' ? redirect()->intended(route('admin')) : redirect()->route('welcome');
     }
 
     /**
