06c8af530a6f84444d747d129905b9b82f5ce00a
Alain Precioso <alainprecioso@gmail.com>
2024-10-31 10:19:46 +0100

    Set the name at the center level of the profile picture in comments

--- resources/views/posts/partials/comment-content.blade.php
+++ resources/views/posts/partials/comment-content.blade.php
@@ -1,76 +1,78 @@
-<div class="border-b border-gray-300 bg-gray-900 py-2 p-2 flex flex-col">
-    <div class="flex items-start space-x-2">
-        <!-- Profile picture -->
-        @if($content->user->profile_picture)
+<div class="border-b border-gray-300 bg-gray-900 py-2 p-2">
+    <div class="flex justify-between items-start">
+        <div class="flex items-center space-x-2">
+            <!-- Profile Picture -->
             <div>
-                <img src="{{ asset('storage/' . $content->user->profile_picture) }}" alt="Profile Picture"
-                     class="w-12 h-12 rounded-full object-cover">
+                @if($content->user->profile_picture)
+                    <img src="{{ asset('storage/' . $content->user->profile_picture) }}" alt="Profile Picture"
+                         class="w-12 h-12 rounded-full object-cover">
+                @else
+                    <img src="{{ asset('storage/profile_picture/default.png')}}" alt="Profile Picture"
+                         class="w-12 h-12 rounded-full object-cover">
+                @endif
             </div>
-        @else
+            <!-- Name -->
             <div>
-                <img src="{{ asset('storage/profile_picture/default.png')}}" alt="Profile Picture"
-                     class="w-12 h-12 rounded-full object-cover">
-            </div>
-        @endif
-
-        <div class="flex-1">
-            <div class="flex justify-between items-center">
-                <!-- Name -->
-                <a href="{{ route('profile.show', ['username' => $content->user->name]) }}" class="erah-link font-bold">
+                <a href="{{ route('profile.show', ['username' => $content->user->name]) }}"
+                   class="erah-link font-bold">
                     {{ $content->user->name }}
                 </a>
-
-                <div>
-                    <!-- Delete Link -->
-                    @if (auth()->user() && (auth()->user()->id === $content->user->id || auth()->user()->isAdmin()))
-                        <form action="{{ route('comments.destroy', $content->id) }}" method="POST" class="inline"
-                              onsubmit="return confirmDelete();">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="text-red-600 hover:underline">
-                                Supprimer
-                            </button>
-                        </form>
-                    @endif
-
-                    <!-- Creation date and link -->
-                    <a href="{{ route('comments.show', ['post' => $content->comment->post->id, 'comment' => $content->id]) }}"
-                       class="hover:underline">
-                        <span class="text-gray-500 text-sm convert-time"
-                              data-time="{{ $content->created_at->toIso8601String() }}"></span>
-                    </a>
-                </div>
             </div>
+        </div>
 
-            <!-- Body -->
-            <div class="py-2">
-                <div id="content-preview-{{ $content->id }}"
-                     class="max-h-36 overflow-hidden transition-all duration-300 ease-in-out">
-                    {!! nl2br(e($content->body)) !!}
-                </div>
-                <span id="toggle-container-{{ $content->id }}" class="hidden">
-                    <button id="toggle-button-more-{{ $content->id }}" class="mt-2 text-blue-600 hover:underline"
-                            onclick="showMore({{ $content->id }})">Dérouler</button>
-                    <button id="toggle-button-less-{{ $content->id }}" class="mt-2 text-blue-600 hover:underline hidden"
-                            onclick="showLess({{ $content->id }})">Cacher</button>
-                </span>
+        <div class="flex space-x-2">
+            <!-- Delete Link -->
+            <div>
+                @if (auth()->user() && (auth()->user()->id === $content->user->id || auth()->user()->isAdmin()))
+                    <form action="{{ route('comments.destroy', $content->id) }}" method="POST" class="inline"
+                          onsubmit="return confirmDelete();">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="text-red-600 hover:underline">
+                            Supprimer
+                        </button>
+                    </form>
+                @endif
             </div>
-
-            @if ($content->media)
-                <div class="py-2">
-                    @if (filter_var($content->media, FILTER_VALIDATE_URL) && strpos($content->media, 'tenor.com') !== false)
-                        <!-- If it's a Tenor GIF URL -->
-                        <img src="{{ $content->media }}" alt="Comment GIF" class="object-contain h-48 w-48">
-                    @else
-                        <!-- If it's an uploaded image -->
-                        <img src="{{ asset('storage/' . $content->media) }}" alt="Comment Image"
-                             class="object-contain h-48 w-48">
-                    @endif
-                </div>
-            @endif
+            <!-- Creation Date and Link -->
+            <div>
+                <a href="{{ route('comments.show', ['post' => $content->comment->post->id, 'comment' => $content->id]) }}"
+                   class="hover:underline">
+                    <span class="text-gray-500 text-sm convert-time"
+                          data-time="{{ $content->created_at->toIso8601String() }}"></span>
+                </a>
+            </div>
         </div>
     </div>
+
+    <!-- Body -->
+    <div class="py-2 ml-14">
+        <div id="content-preview-{{ $content->id }}"
+             class="max-h-36 overflow-hidden transition-all duration-300 ease-in-out">
+            {!! nl2br(e($content->body)) !!}
+        </div>
+        <span id="toggle-container-{{ $content->id }}" class="hidden">
+            <button id="toggle-button-more-{{ $content->id }}" class="mt-2 text-blue-600 hover:underline"
+                    onclick="showMore({{ $content->id }})">Dérouler</button>
+            <button id="toggle-button-less-{{ $content->id }}" class="mt-2 text-blue-600 hover:underline hidden"
+                    onclick="showLess({{ $content->id }})">Cacher</button>
+        </span>
+    </div>
+
+    @if ($content->media)
+        <div class="py-2">
+            @if (filter_var($content->media, FILTER_VALIDATE_URL) && strpos($content->media, 'tenor.com') !== false)
+                <!-- If it's a Tenor GIF URL -->
+                <img src="{{ $content->media }}" alt="Comment GIF" class="object-contain h-48 w-48">
+            @else
+                <!-- If it's an uploaded image -->
+                <img src="{{ asset('storage/' . $content->media) }}" alt="Comment Image"
+                     class="object-contain h-48 w-48">
+            @endif
+        </div>
+    @endif
 </div>
+
 
 <script>
     function confirmDelete() {
