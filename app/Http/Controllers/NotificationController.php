ce6f80b5dc21fffb89e77cc7322f4ae9b6135370
Alain Precioso <alainprecioso@gmail.com>
2024-11-12 11:29:26 +0100

    Reject the notifications from the array if they are deleted

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -13,51 +13,54 @@ class NotificationController extends Controller
     {
         $notifications = auth()->user()->notifications()->latest()->get();
 
-        $notifications->each(function ($notification) {
-
+        $notifications = $notifications->reject(function ($notification) {
             // Post Published Notifications
             if ($notification->type === 'App\Notifications\PostPublishedNotification') {
-                // Get the post related to the notification
                 $post = Post::find($notification->data['post_id']);
                 if (!$post) {
                     $notification->delete();
+                    return true;
                 } else {
-                    // Attach the post to the notification object
                     $notification->post = $post;
                 }
 
-            } // Comment Reply Notifications
-            elseif ($notification->type === 'App\Notifications\CommentReplyNotification') {
+            } elseif ($notification->type === 'App\Notifications\CommentReplyNotification') {
                 $comment = Comment::find($notification->data['comment_id']);
                 if (!$comment || !$comment->contentExists()) {
                     $notification->delete();
+                    return true;
                 } else {
                     $notification->comment = $comment;
                 }
-            } // Comment Like Notifications
-            elseif ($notification->type === 'App\Notifications\CommentLikeNotification') {
+
+            } elseif ($notification->type === 'App\Notifications\CommentLikeNotification') {
                 $likeIds = $notification->data['ids'] ?? [];
                 $likes = Like::whereIn('id', $likeIds)->get();
 
                 if ($likes->isEmpty()) {
                     $notification->delete();
+                    return true;
                 } else {
                     $notification->likes = $likes;
                     $notification->like_count = $notification->data['count'] ?? count($likeIds);
                 }
-            } // Follow Notifications
-            elseif ($notification->type === 'App\Notifications\FollowNotification') {
+
+            } elseif ($notification->type === 'App\Notifications\FollowNotification') {
                 $followIds = $notification->data['ids'] ?? [];
                 $follows = Follow::whereIn('id', $followIds)->get();
 
                 if ($follows->isEmpty()) {
                     $notification->delete();
+                    return true;
                 } else {
                     $notification->follows = $follows;
                     $notification->follow_count = $notification->data['count'] ?? count($followIds);
                 }
             }
+
+            return false; // Keep in collection
         });
+
 
         return view('notifications.index', compact('notifications'));
     }
