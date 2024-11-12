86102775d4dbc2ba6236f56006736d3c8f1a8d4d
Alain Precioso <alainprecioso@gmail.com>
2024-11-12 13:17:05 +0100

    Hide the notification bubble if there's no notifications

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -62,7 +62,10 @@
                     <a href="{{ route('notifications.index') }}" class="relative inline-flex items-center p-3 text-sm font-medium text-center text-white rounded-lg">
                         <x-svg-bell />
                         <span class="sr-only">Notifications</span>
-                        <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-1 -end-1 dark:border-gray-900">{{ Auth::user()->unreadNotificationsCount() }}</div>
+                        @if( Auth::user()->unreadNotificationsCount() > 0 )
+                            <div
+                                class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-1 -end-1 dark:border-gray-900">{{ Auth::user()->unreadNotificationsCount() }}</div>
+                        @endif
                     </a>
 
                     <!-- Settings Dropdown -->
