b73db34b5d0d7fa0118ab2a5bcea0c141b901fa7
Alain Precioso <alainprecioso@gmail.com>
2024-11-13 09:08:36 +0100

    Move the bundled notification contracts to the right interface

--- app/Contracts/BundledNotification.php
+++ app/Contracts/BundledNotification.php
@@ -4,4 +4,28 @@ namespace App\Contracts;
 
 interface BundledNotification extends NotifiableEntityInterface
 {
+    /**
+     * Get the notification class for this model.
+     *
+     * @return string
+     */
+    public function getNotificationClass(): string;
+
+    /**
+     * Get the notification type for this model (e.g., 'comment_like' or 'follow').
+     *
+     * @return string
+     */
+    public function getNotificationType(): ?string;
+
+    /**
+     * Get the context ID for this model (e.g., comment_id or user_id).
+     *
+     * @return int
+     */
+    public function getContextId();
+
+    public function getContextType(): ?string;
+
+    public function targetUser();
 }
