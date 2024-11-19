d0439c6b7796dfc0a1effacbba6621f76d44c6fa
Alain Precioso <alainprecioso@gmail.com>
2024-11-19 12:53:11 +0100

    Reduce comment tree lines size on small screens

--- resources/views/posts/partials/comment-structure.blade.php
+++ resources/views/posts/partials/comment-structure.blade.php
@@ -1,7 +1,7 @@
 <div class="border-l border-gray-300 py-2">
     <div class="flex items-start">
         <!-- Tree lines -->
-        <div class="border-b border-gray-300 py-2 flex-shrink-0 w-12 h-10"></div>
+        <div class="border-b border-gray-300 py-2 flex-shrink-0 w-5 h-10 md:w-12"></div>
 
         <!-- Comment -->
         @include('posts.partials.comment', ['comment' => $comment, 'depth' => $depth])
