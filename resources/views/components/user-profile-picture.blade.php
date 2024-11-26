81ea352aaa6ffe9ff4dfdc2c7ae2330888791b45
Alain Precioso <alainprecioso@gmail.com>
2024-11-26 13:50:17 +0100

    Display a big and small profile picture on profile edit page

--- resources/views/components/user-profile-picture.blade.php
+++ resources/views/components/user-profile-picture.blade.php
@@ -1,17 +1,20 @@
-@props(['user', 'default' => true])
+@props(['user', 'default' => true, 'size' => '12' , 'border' => true])
 
 @php
-    $class = match ($user->role) {
-        'admin' => 'border-2 border-red-500 shadow-lg',
-        'ultra' => 'border-2 border-yellow-500 shadow-lg',
-        default => 'border-2 border-gray-300',
-    };
+    $class = '';
+    if ($border) {
+        $class = match ($user->role) {
+            'admin' => 'border-2 border-red-500 shadow-lg',
+            'ultra' => 'border-2 border-yellow-500 shadow-lg',
+            default => 'border-2 border-gray-300',
+        };
+    }
 @endphp
 
 @if($user->profile_picture)
     <img src="{{ asset('storage/' . $user->profile_picture) }}" alt="Profile Picture"
-         class="w-12 h-12 rounded-full object-cover {{ $class }}">
+         class="w-{{ $size }} h-{{ $size }} rounded-full object-cover {{ $class }}">
 @elseif($default)
     <img src="{{ asset('storage/profile_pictures/default.png')}}" alt="Default Profile Picture"
-         class="w-12 h-12 rounded-full object-cover {{ $class }}">
+         class="w-{{ $size }} h-{{ $size }} rounded-full object-cover {{ $class }}">
 @endif
