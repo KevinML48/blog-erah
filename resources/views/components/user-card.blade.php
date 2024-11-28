b9f8364d8d7028d69265f73c3cea8c7472873f9a
Alain Precioso <alainprecioso@gmail.com>
2024-11-28 07:49:31 +0100

    Add auth check to cards

--- resources/views/components/user-card.blade.php
+++ resources/views/components/user-card.blade.php
@@ -10,30 +10,31 @@
 
         <!-- Name -->
         <div class="text-left ml-2">
-            <x-username :user="$user" />
+            <x-username :user="$user"/>
         </div>
     </div>
 
     <!-- Follow/Unfollow Button Section (Positioned Top Right) -->
-    @if(Auth::user()->id != $user->id)
-        <div class="absolute top-0 right-0 mt-2 mr-2">
-            <button id="unfollow-button-{{ $user->id }}"
-                    class="unfollow-button-{{ $user->id }} follow-button inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 transition ease-in-out duration-150 {{ auth()->user()->isFollowing($user) ? '' : 'hidden' }}"
-                    data-following="true"
-                    onclick="unfollowUser({{ $user->id }})"
-                    data-user-id="{{ $user->id }}">
-                Se désabonner
-            </button>
-            <button id="follow-button-{{ $user->id }}"
-                    class="follow-button-{{ $user->id }} follow-button inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 transition ease-in-out duration-150 {{ auth()->user()->isFollowing($user) ? 'hidden' : '' }}"
-                    data-following="false"
-                    onclick="followUser({{ $user->id }})"
-                    data-user-id="{{ $user->id }}">
-                S'abonner
-            </button>
-        </div>
-    @endif
-
+    @Auth
+        @if(Auth::user()->id != $user->id)
+            <div class="absolute top-0 right-0 mt-2 mr-2">
+                <button id="unfollow-button-{{ $user->id }}"
+                        class="unfollow-button-{{ $user->id }} follow-button inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 transition ease-in-out duration-150 {{ auth()->user()->isFollowing($user) ? '' : 'hidden' }}"
+                        data-following="true"
+                        onclick="unfollowUser({{ $user->id }})"
+                        data-user-id="{{ $user->id }}">
+                    Se désabonner
+                </button>
+                <button id="follow-button-{{ $user->id }}"
+                        class="follow-button-{{ $user->id }} follow-button inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 transition ease-in-out duration-150 {{ auth()->user()->isFollowing($user) ? 'hidden' : '' }}"
+                        data-following="false"
+                        onclick="followUser({{ $user->id }})"
+                        data-user-id="{{ $user->id }}">
+                    S'abonner
+                </button>
+            </div>
+        @endif
+    @endauth
     <!-- Description -->
     <div class="mt-2 text-left text-sm text-gray-600 px-4">
         {{ $user->description }}
